cacccb76d3e4028f7dc816f74628045d2bc5ca16
blakkkk <23288358+blakkkk@users.noreply.github.com>
2022-03-25 09:14:19 +1000

    lesson 5

---
+++ lessons/L5_Ex1.py
@@ -0,0 +1,8 @@
+# Alfred's security guard program
+
+#####################################################
+## Write a program that asks for a person's name   ##
+## and then grants entry of that person is Alfred ##
+## everyone else is told, politely, to go away     ##
+#####################################################
+
