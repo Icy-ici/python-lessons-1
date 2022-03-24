bd4f975de1cf1e0d8261bd9ee4d06c7a9b17848e
blakkkk <23288358+blakkkk@users.noreply.github.com>
2022-03-25 09:25:14 +1000

    lesson 5

--- lessons/L5_Ex1.py
+++ lessons/L5_Ex1.py
@@ -2,7 +2,13 @@
 
 #####################################################
 ## Write a program that asks for a person's name   ##
-## and then grants entry of that person is Alfred ##
+## and then grants entry of that person is Alfred  ##
 ## everyone else is told, politely, to go away     ##
 #####################################################
 
+name = input("What is your name? ") ## Ask for the name
+
+if name == "Alfred": ## If the name is Alfred,
+    print("Welcome!") ## Welcome
+else: ## If the name is not Alfred,
+    print("You are not Alfred, please leave.") ## Tell them to leave
